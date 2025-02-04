f84fa80ec9468e01a4dae3a32571f5ef56a4699c
Abraham <scrturbocx@gmail.com>
2025-02-04 14:40:13 -0400

    UWU
    XD

---
+++ 11.py
@@ -0,0 +1,11 @@
+nombre= input("por favor, ingrese tu nombre: ")
+print(f"hola, {nombre}!")
+
+
+
+
+edad=int(input("por favor, ingrese el numoero del mes en que se morira: "))
+print(f"te moriras la fecha {edad} de este mes, ")
+
+altura=float(input("por favor ingre la altura en la que te quieres morir en los rascacielos: "))
+print(f"Caeras {altura} metros de altura")
---
+++ 13.py
@@ -0,0 +1,4 @@
+nombre = int(input("numero 1"))
+edad = int(input("numero 2"))
+a = nombre + edad
+print(f"la sumatoria de, {nombre}. mas {edad} es {a}")
