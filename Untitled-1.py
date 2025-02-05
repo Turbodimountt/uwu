5d4eb358442746dcd500f8066f33fbf78d8775a1
Turbodismountt <scrturbocx@gmail.com>
2025-02-04 23:54:22 -0400

    Agregar script para solicitar información sobre la fecha y lugar de muerte

---
+++ Untitled-1.py
@@ -0,0 +1,4 @@
+nombre = input("por favor, dime el dia en que quieres morir")
+edad = int(input("por favor, ingrese el numero del dia del mes"))
+ciudad = input("en que ciudad quieres morir?")
+print(f"moriras el dia, {nombre}. de tal fecha {edad} solo y moriras en {ciudad}.")
