092062b57019fc4f87975c20a2dbd038c98f8d27
Abraham <scrturbocx@gmail.com>
2025-02-05 16:48:15 -0400

    Add files via upload

---
+++ problema.py
@@ -0,0 +1,29 @@
+print ("\nbienvenidos a depresion landia, lugar donde uno compra sufirmiento ?")
+nombre = []
+while True:
+    print ("1 agregar articulo")
+    print ("2 ver articulos del carrito")
+    print ("3 remover articulos del carrito")
+    print ("4 consulta todal del carrito")
+    print ("5 cerrar programa")
+    
+    elegir = input("que articulo deseas llevar?" )
+    if elegir == 1:
+        nombre = input("cual es el nombre del producto?")
+        nombre.append(nombre)
+    print("\nse ha agregado el producto")
+    elegir = input("deseas eliminar el prodcuto")
+    if elegir == 2:
+    if len(nombre) == 0:
+       print("no se encontraron productos")
+     if:
+       print("ver articulo:")
+    for i in range(len(nombre)):
+        print(f"{i+1}.{nombre[1])
+         nombre = input("cual es el nombre del producto?")
+         nombre.pop(nombre)
+    print("\nse ha desintegrado el producto")     
+    break
+
+ 
+  
